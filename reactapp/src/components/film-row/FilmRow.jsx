16c16f51074fd321f3586c20d9db0713e32a20f4
ahmedalhindi <alhindi.ahmed@gmail.com>
2020-01-05 13:00:38 +0300

    step7

--- reactapp/src/components/film-row/FilmRow.jsx
+++ reactapp/src/components/film-row/FilmRow.jsx
@@ -4,14 +4,16 @@ class FilmRow extends Component{
     render(){
         let baseURL = "https://image.tmdb.org/t/p/w780/";
         let posterUrl = this.props.film.poster_path
-        console.log(posterUrl)
+        // console.log(posterUrl)
+        let rDate = new Date(this.props.film.release_date);
+        rDate = rDate.getFullYear();
         return(
             <div className="film-row">
                 <img src={baseURL+posterUrl} alt="" />
 
                 <div className="film-summary">
                     <h1>{this.props.film.title}</h1>
-                    <p>{this.props.film.release_date}</p>
+                    <p>{rDate}</p>
                 </div>
             </div>
         )
